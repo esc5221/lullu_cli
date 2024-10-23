2f8cc76ea3576a8687dee7360c839652c8468cf7
최병욱 <esc5221@gmail.com>
2024-10-23 23:41:35 +0900

    fix: register save_context command

--- commands/py/__init__.py
+++ commands/py/__init__.py
@@ -1,11 +1,12 @@
 import os
 import typer
 
-from .copy_context import copy_context, copy_import
+from .copy_context import copy_context, copy_import, save_context
 from .paste_llm import paste_llm
 
 app = typer.Typer()
 
 app.command()(copy_context)
 app.command()(copy_import)
+app.command()(save_context)
 app.command()(paste_llm)
